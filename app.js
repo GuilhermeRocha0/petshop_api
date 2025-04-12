3bcb2fca75dd1eda63a7a12fcc2ab89d8f5277f0
GuilhermeRocha0 <gui.rocha009@gmail.com>
2025-04-12 13:11:45 -0300

    feat: create user

--- app.js
+++ app.js
@@ -9,9 +9,76 @@ const app = express()
 // Config JSON response
 app.use(express.json())
 
+// Models
+const User = require('./models/User')
+
+// Utilites
+const isValidCPF = require('./utilities/isValidCPF')
+
 // Open Route - Public Route
 app.get('/', (req, res) => {
   res.status(200).json({ msg: 'Bem vindo a PetShop API' })
+})
+
+// Register User
+app.post('/auth/register', async (req, res) => {
+  const { name, cpf, email, password, confirmPassword } = req.body
+
+  // validations
+  if (!name) {
+    return res.status(422).json({ msg: 'O nome é obrigatório!' })
+  }
+
+  if (!cpf) {
+    return res.status(422).json({ msg: 'O CPF é obrigatório!' })
+  }
+
+  if (!email) {
+    return res.status(422).json({ msg: 'O email é obrigatório!' })
+  }
+
+  if (!password) {
+    return res.status(422).json({ msg: 'A senha é obrigatória!' })
+  }
+
+  if (password !== confirmPassword) {
+    return res.status(422).json({ msg: 'As senhas não conferem!' })
+  }
+
+  // check if cpf is valid
+  if (!isValidCPF(cpf)) {
+    return res.status(422).json({ msg: 'CPF inválido, utilize um CPF válido!' })
+  }
+
+  // check if user exists
+  const userExists = await User.findOne({ email: email })
+  if (userExists) {
+    return res.status(422).json({ msg: 'Usuário com este email já existe!' })
+  }
+
+  // create password
+  const salt = await bcrypt.genSalt(12)
+  const passwordHash = await bcrypt.hash(password, salt)
+
+  // create user
+  const user = new User({
+    name,
+    cpf,
+    email,
+    password: passwordHash
+  })
+
+  try {
+    await user.save()
+
+    res.status(201).json({ msg: 'Usuário criado com sucesso!' })
+  } catch (error) {
+    console.log(error)
+
+    res
+      .status(500)
+      .json({ msg: 'Ocorreu um erro no servidor, tente novamente mais tarde!' })
+  }
 })
 
 // Credentials
---
+++ utilities/isValidCPF.js
@@ -0,0 +1,31 @@
+function isValidCPF(cpf) {
+  cpf = cpf.replace(/[^\d]+/g, '')
+
+  if (cpf.length !== 11 || /^(\d)\1+$/.test(cpf)) {
+    return false
+  }
+
+  let sum = 0
+  for (let i = 0; i < 9; i++) {
+    sum += parseInt(cpf[i]) * (10 - i)
+  }
+
+  let firstDigit = 11 - (sum % 11)
+  if (firstDigit >= 10) firstDigit = 0
+
+  if (firstDigit !== parseInt(cpf[9])) {
+    return false
+  }
+
+  sum = 0
+  for (let i = 0; i < 10; i++) {
+    sum += parseInt(cpf[i]) * (11 - i)
+  }
+
+  let secondDigit = 11 - (sum % 11)
+  if (secondDigit >= 10) secondDigit = 0
+
+  return secondDigit === parseInt(cpf[10])
+}
+
+module.exports = isValidCPF
