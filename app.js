f22481fe88983075a00ef8908b7760a3dae57843
GuilhermeRocha0 <gui.rocha009@gmail.com>
2025-04-12 12:22:37 -0300

    ops: configure credenctials and db connection

--- app.js
+++ app.js
@@ -6,8 +6,27 @@ const jwt = require('jsonwebtoken')
 
 const app = express()
 
+// Config JSON response
+app.use(express.json())
+
+// Open Route - Public Route
 app.get('/', (req, res) => {
   res.status(200).json({ msg: 'Bem vindo a PetShop API' })
 })
+
+// Credentials
+const dbUser = process.env.DB_USER
+const dbPassword = process.env.DB_PASS
+
+mongoose
+  .connect(
+    `mongodb+srv://${dbUser}:${dbPassword}@cluster0.9e7tkyp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
+  )
+  .then(() => {
+    console.log('Conectou ao banco!')
+  })
+  .catch(err => {
+    console.log(err)
+  })
 
 app.listen(3000)
